f0921150d745056dfe0e567e948d96a7192e1c5b
ABUBAKAR OSMAN <abubakarosman@ABUBAKARs-MacBook-Pro.local>
2023-10-19 03:32:12 +0000

    Installation of vite-react and tailwindcss and creation of the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-[#530C0C] w-full h-[100vh] flex justify-center items-center">
+      <h1 className="text-white font-mono font-extrabold text-4xl">ASHESI iCONNECT</h1>
+    </div>
+  )
+}
+
+export default App
